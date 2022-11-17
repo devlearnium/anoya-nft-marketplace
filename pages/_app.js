1b15c8f86dd1f7a795a15e41bf86ee59c7925eba
devlearnium <devlearnium@gmail.com>
2022-11-17 22:34:42 +0400

    _app fixes

--- pages/_app.js
+++ pages/_app.js
@@ -9,25 +9,17 @@ import Footer from '../components/Footer'
 
 
 function MyApp({ Component, pageProps }) {
-    return ( <
-        div >
-        <
-        Head >
-        <
-        title > ANOYA - NFT Marketplace < /title> <
-        meta name = "description"
+    return ( <div>
+        <Head>
+        <title> ANOYA - NFT Marketplace </title> 
+        <meta name = "description"
         content = "Anftiz is a full-fledged NFT or Digital assets marketplace HTML template. It is suitable for selling NFTs, or non-fungible tokens, Digital Arts, Digital Music & All types of Digital Assets. You can create, sell & collect NFT’s or digital assets through this website." / >
-        <
-        link rel = "icon"
+        <link rel = "icon"
         type = "image/png"
         href = "assets/images/favicon.png" / >
-        <
-        /Head> <
-        Header > < /Header> <
-        Component {...pageProps }
-        /> <
-        Footer > < /Footer> <
-        /div>
+        </Head> 
+        <Header> </Header> <
+        Component {...pageProps }/> <Footer > </Footer> </div>
 
     )
 }
