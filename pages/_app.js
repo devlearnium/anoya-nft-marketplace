1f89960128f24f9e7191184b35796731637adda3
devlearnium <devlearnium@gmail.com>
2022-11-17 20:43:23 +0400

    change text

--- pages/_app.js
+++ pages/_app.js
@@ -9,19 +9,27 @@ import Footer from '../components/Footer'
 
 
 function MyApp({ Component, pageProps }) {
-  return (
-  <div>
-    <Head>
-      <title>Anftiz- NFT Marketplace React Next.js Template</title>
-      <meta name="description" content="Anftiz is a full-fledged NFT or Digital assets marketplace HTML template. It is suitable for selling NFTs, or non-fungible tokens, Digital Arts, Digital Music & All types of Digital Assets. You can create, sell & collect NFT’s or digital assets through this website." />
-      <link rel="icon" type="image/png" href="assets/images/favicon.png" />
-    </Head>
-    <Header></Header>
-    <Component {...pageProps} />
-    <Footer></Footer>
-  </div>
-  
-  )
+    return ( <
+        div >
+        <
+        Head >
+        <
+        title > ANOYA - NFT Marketplace < /title> <
+        meta name = "description"
+        content = "ANOYA is a full-fledged NFT  marketplace for selling NFTs, or non-fungible tokens, Digital Arts, Digital Music & All types of Digital Assets. You can create, sell & collect NFT’s or digital assets through this website." / >
+        <
+        link rel = "icon"
+        type = "image/png"
+        href = "assets/images/favicon.png" / >
+        <
+        /Head> <
+        Header > < /Header> <
+        Component {...pageProps }
+        /> <
+        Footer > < /Footer> < /
+        div >
+
+    )
 }
 
 export default MyApp
