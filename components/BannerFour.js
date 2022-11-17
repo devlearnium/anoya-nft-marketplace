767a9c5bc07dfe428e41cf38973185439c250150
devlearnium <devlearnium@gmail.com>
2022-11-17 21:56:29 +0000

    Update BannerFour.js

--- components/BannerFour.js
+++ components/BannerFour.js
@@ -94,7 +94,7 @@ const BannerFour = () => {
                 </div>
             </div>
         </div>
-        <iframe src="https://rive.app/community/3146-6725-pull-to-refresh/embed" width={auto} height={300} ></iframe>
+        <iframe src="https://rive.app/community/3146-6725-pull-to-refresh/embed" width={1000} height={300} ></iframe>
     </section>
     
     )
